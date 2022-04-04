5b0eea4776abb9da85fce385c15e6d4dfcdb4664
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-04 16:32:48 +0500

    Added abstraction

--- animals.rb
+++ animals.rb
@@ -1,4 +1,4 @@
-class Animals
+class Animal
   def initialize(type, number_of_legs, name = 'Unknown name')
     @id = Random.rand(1..1000)
     @type = type
@@ -7,4 +7,13 @@ class Animals
   end
   attr_accessor :name
   attr_reader :id, :type, :number_of_legs
+
+  def speak
+    case @type
+    when 'dog'
+      'Woof, woof'
+    when 'spider'
+      '...'
+    end
+  end
 end
